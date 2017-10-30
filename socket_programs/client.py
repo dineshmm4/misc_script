f38384bb59cd951d606c9ac475f5c9b2b8e6f36b
root <root@cs002.local.host.com>
2017-10-30 13:47:17 +0530

    client script socket program

--- socket_programs/client.py
+++ socket_programs/client.py
@@ -2,11 +2,16 @@
 import socket
 
 s = socket.socket()
-host = socket.gethostname() 
+host = 'cs001.local.host.com'
 port = 12345
 
 s.connect((host,port))
 print (s.recv(1024))
+cmd = 'ping'
+s.send(cmd)
+result = s.recv(1024)
+if result == 'pong':
+	print(result)
 
 s.close()
 
